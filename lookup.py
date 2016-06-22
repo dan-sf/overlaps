27a0d8a6daa422e9336700e4453e4c67ddc76d71
dfowler <dfowler@compete.com>
2016-06-21 19:35:52 -0700

    refactor lookup.py

--- lookup.py
+++ lookup.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python
 
-#---------------
-# Overlap lookup
-#---------------
-
 import sys
 import argparse
 
-# Parse the args
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", action="store", help="One column file to lookup against", dest="file")
-parser.add_argument("-c", "--column", action="store", help="Zero based column number to lookup against", dest="column")
-parser.add_argument("-i", "--include", action="store", help="One or zero to include or disallow values from the lookup file", dest="include")
-args = vars(parser.parse_args())
+def lookup_set(lookup_file):
+    """
+    Create a set from the lookup_file
+    """
+    return { line.rstrip('\n') for line in open(lookup_file) }
 
-# Create lookup set of values from the input file
-lookup_set = set(line.strip() for line in open(args['file']))
+def filter_data(stream, column, include, lookup_file):
+    """
+    Include or disallow records that match data in lookup_file
+    """
+    join_set = lookup_set(lookup_file)
+    for line in stream:
+        line = line.rstrip('\n').split('\t')
+        if include and line[column] in join_set:
+            print '\t'.join(str(x) for x in line)
+        elif not include and line[column] not in join_set:
+            print '\t'.join(str(x) for x in line)
 
-# Loop through stdin looking up against the input values
-for row in sys.stdin:
-	row_list = list(row.strip().split('\t'))
-	if int(args['include']) == 1 and row_list[int(args['column'])] in lookup_set:
-		print '\t'.join(str(x) for x in row_list)
-	elif int(args['include']) != 1 and row_list[int(args['column'])] not in lookup_set:
-		print '\t'.join(str(x) for x in row_list)
+def cmd_line_parser(args):
+    """
+    Parse cmd line args
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-f", "--file", action="store",
+                        help="One column file to lookup against", dest="file")
+    parser.add_argument("-c", "--column", action="store", type=int,
+                        help="Zero based column number to lookup against", dest="column")
+    parser.add_argument("-i", "--include", action="store_true",
+                        help="One or zero to include or disallow values from the lookup file", dest="include")
+    return parser.parse_args(args)
+
+def main():
+    """
+    Run the main task
+    """
+    args = cmd_line_parser(sys.argv[1:])
+    filter_data(sys.stdin, args.column, args.include, args.file)
+
+if __name__ == '__main__':
+    main()
 
