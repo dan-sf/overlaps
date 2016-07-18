f6d4769ae47754bedc2c25ae57f73a1f43245446
dfowler <dfowler@compete.com>
2016-07-17 22:21:13 -0700

    update options add print_data function

--- lookup.py
+++ lookup.py
@@ -1,7 +1,12 @@
-#!/usr/bin/python
-
 import sys
 import argparse
+
+def print_data(row):
+    """
+    Write data to stdout
+    """
+    row = '\t'.join(str(i) for i in row)
+    sys.stdout.write(row + '\n')
 
 def lookup_set(lookup_file):
     """
@@ -9,17 +14,17 @@ def lookup_set(lookup_file):
     """
     return { line.rstrip('\n') for line in open(lookup_file) }
 
-def filter_data(stream, column, include, lookup_file):
+def filter_data(stream, args):
     """
     Include or disallow records that match data in lookup_file
     """
-    join_set = lookup_set(lookup_file)
+    join_set = lookup_set(args.file)
     for line in stream:
         line = line.rstrip('\n').split('\t')
-        if include and line[column] in join_set:
-            print '\t'.join(str(x) for x in line)
-        elif not include and line[column] not in join_set:
-            print '\t'.join(str(x) for x in line)
+        if not args.remove and line[args.column] in join_set:
+            print_data(line)
+        elif args.remove and line[args.column] not in join_set:
+            print_data(line)
 
 def cmd_line_parser(args):
     """
@@ -31,8 +36,8 @@ def cmd_line_parser(args):
                         help="One column file to lookup against", dest="file")
     parser.add_argument("-c", "--column", action="store", type=int,
                         help="Zero based column number to lookup against", dest="column")
-    parser.add_argument("-i", "--include", action="store_true",
-                        help="One or zero to include or disallow values from the lookup file", dest="include")
+    parser.add_argument("-r", "--remove", action="store_true", default=False,
+                        help="Disallow values from the lookup file", dest="remove")
     return parser.parse_args(args)
 
 def main():
@@ -40,7 +45,7 @@ def main():
     Run the main task
     """
     args = cmd_line_parser(sys.argv[1:])
-    filter_data(sys.stdin, args.column, args.include, args.file)
+    filter_data(sys.stdin, args)
 
 if __name__ == '__main__':
     main()
