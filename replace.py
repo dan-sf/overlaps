10a0e592823287a056ecfcb5d9b7d9cf51620c93
dfowler <dfowler@compete.com>
2016-07-17 22:02:14 -0700

    refactor replace.py

--- replace.py
+++ replace.py
@@ -1,36 +1,63 @@
-#!/usr/bin/python
-
-#----------------
-# Overlap replace
-#----------------
-
 import sys
 import argparse
 
-# Parse the args
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", action="store", help="Two column file to lookup against", dest="file")
-parser.add_argument("-c", "--column", action="store", help="Zero based column number to lookup against", dest="column")
-parser.add_argument("-r", "--replace", action="store", help="One or zero to replace or append values from the lookup file (1: replace, 2: append)", dest="replace")
-parser.add_argument("-p", "--printall", action="store", help="Print non-matching values with input string", dest="printall")
-args = vars(parser.parse_args())
+def print_data(row):
+    """
+    Write data to stdout
+    """
+    row = '\t'.join(str(x) for x in row)
+    sys.stdout.write(row + '\n')
 
-# # Create lookup dictionary of values from the input file
-lookup_dic = {}
-for line in open(args['file']):
-	line_list = line.strip().split('\t')
-	lookup_dic[line_list[0]] = line_list[1]
+def create_lookup(lookup_file):
+    """
+    Create lookup dictionary of values from the input file
+    """
+    lookup_dict = {}
+    for line in open(lookup_file):
+            line_list = line.rstrip('\n').split('\t')
+            lookup_dict[line_list[0]] = line_list[1]
+    return lookup_dict
 
-# Loop through stdin looking up against the input values replacing or appending matching records
-for row in sys.stdin:
-	row_list = list(row.strip().split('\t'))
-	if int(args['replace']) == 1 and row_list[int(args['column'])] in lookup_dic:
-		row_list[int(args['column'])] = lookup_dic[row_list[int(args['column'])]]
-		print '\t'.join(str(x) for x in row_list)
-	elif int(args['replace']) == 0 and row_list[int(args['column'])] in lookup_dic:
-		row_list.append(lookup_dic[row_list[int(args['column'])]])
-		print '\t'.join(str(x) for x in row_list)
-	elif row_list[int(args['column'])] not in lookup_dic and args['printall'] != None:
-		row_list.append(args['printall'])
-		print '\t'.join(str(x) for x in row_list)
+def join_data(stream, args):
+    """
+    Join the data, either appending or replacing values
+    """
+    join_dict = create_lookup(args.file)
+    for row in stream:
+        row = row.rstrip('\n').split('\t')
+        if args.replace and row[ args.column ] in join_dict:
+            row[ args.column ] = join_dict[row[ args.column ]]
+            print_data(row)
+        elif not args.replace and row[ args.column ] in join_dict:
+            row.append(join_dict[row[ args.column ]])
+            print_data(row)
+        elif row[ args.column ] not in join_dict and args.printall != None:
+            row.append(args.printall)
+            print_data(row)
+
+def cmd_line_parser(args):
+    """
+    Parse cmd line args
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", action="store",
+                        help="Two column file to lookup against", dest="file")
+    parser.add_argument("-c", "--column", action="store", type=int,
+                        help="Zero based column number to lookup against", dest="column")
+    parser.add_argument("-r", "--replace", action="store_true", default=False,
+                        help="Option to replace the field you are matching on", dest="replace")
+    parser.add_argument("-p", "--printall", action="store",
+                        help="Print non-matching values with input string", dest="printall")
+
+    return parser.parse_args(args)
+
+def main():
+    """
+    Run the main task
+    """
+    args = cmd_line_parser(sys.argv[1:])
+    join_data(sys.stdin, args)
+
+if __name__ == '__main__':
+    main()
 
